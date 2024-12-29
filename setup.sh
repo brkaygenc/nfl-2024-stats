1fa8934e791fcfbcd6801a9b10e5368279b0ec7b
Berkay Genc <youngtze@192.168.1.110>
2024-12-29 15:24:02 +0300

    Update Streamlit configuration

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 port = $PORT\n\
+address = \"0.0.0.0\"\n\
 enableCORS = false\n\
-headless = true\n\
-\n\
+enableXsrfProtection = false\n\
 " > ~/.streamlit/config.toml 
